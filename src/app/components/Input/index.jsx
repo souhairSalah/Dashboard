979ebb67bd0310183d802efb05ee1b4e295a7f43
souhair salah <sou6sal@gmail.com>
2023-07-17 11:11:44 +0300

    input component

--- src/app/components/Input/index.jsx
+++ src/app/components/Input/index.jsx
@@ -1,15 +1,36 @@
 import React from "react";
 
-const Input = ({ 
+const Input = ({
+  haveLabel = false,
   label,
   className,
   inputSize = "medium",
   type = "text",
-  error = false
-})=>{
-  return( 
-    <input></input>
+  error = false,
+}) => {
+  let classNames = `border border-sky-500 ${className}`;
+  if (inputSize === "large") {
+    classNames += " py-4 px-5 ";
+  } else if (inputSize === "small") {
+    classNames += " py-2 px-3 ";
+  } else {
+    classNames += " py-3 px-4 ";
+  }
+  if(error){
+    classNames += " border border-red-600";
+  }
+  return ( 
+    <>
+      { haveLabel ? <label>{label}</label> : null }   
+      <input 
+        type={type}
+        inputSize={inputSize}
+        className={classNames}
+        error={error}
+
+      ></input>
+    </>
   );
-}
+};
 
 export default Input;
--- src/app/features/authentication/components/SignInForm/index.jsx
+++ src/app/features/authentication/components/SignInForm/index.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
 import Button from "../../../../components/Button";
+import Input from "../../../../components/Input";
 
 
 const SignInForm = () => {
   return (
     <div>
+      <Input 
+        type="text"
+        inputSize="large"
+        className={"rounded-lg"} 
+        error = {true} 
+        haveLabel = {true} 
+        label = "first name"
+      >
+      </Input>
+      <Input 
+        type="password"
+        inputSize="large"
+        error = {false} 
+        label = "last name" // won't display
+      >
+      </Input>
       
         <Button
          className="bg-emerald-500"
